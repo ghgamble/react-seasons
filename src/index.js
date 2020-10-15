3e18c7847da522cfaa976f5644fcc7f0ab572385
gracehg <grace@statusforward.com>
2020-10-15 12:41:42 -0400

    functionality for SeasonDisplay component

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-      // constructor(props) {
-      //       super(props);
-      //       this.state = { lat: null, errorMessage: '' };
-      // }
 
       state = { lat: null, errorMessage: '' };
 
@@ -23,10 +19,11 @@ class App extends React.Component {
                   return <div>Error: {this.state.errorMessage}</div>;
             }
             if (!this.state.errorMessage && this.state.lat) {
-                  return <SeasonDisplay>Latitude: {this.state.lat}</SeasonDisplay>;
+                  return <SeasonDisplay lat={this.state.lat} />;
             }
             return <div>Loading!</div>;
       }
+
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
