43a2e7c97bc400064d983adae2e0f1c18862daaa
gracehg <grace@statusforward.com>
2020-10-15 11:57:33 -0400

    added state

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,10 +1,7 @@
 import React from 'react';
 
 const SeasonDisplay = () => {
-      window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
-      );
+
       return (
             <div>Season Display</div>
       );
--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import SeasonDisplay from './SeasonDisplay';
+// import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-      render() {
-            window.navigator.geolocation.getCurrentPosition(
-                  (position) => console.log(position),
-                  (err) => console.log(err)
-            );
+      constructor(props) {
+            // Have to call this at the beginning of constructor, connects with React.Component object
+            super(props);
+            // Only time we do direct assignment to this.state, otherwise it's setState
+            this.state = { lat: null, errorMessage: '' };
 
-            return <div>Latitude:</div>;
+            window.navigator.geolocation.getCurrentPosition(
+                  (position) => this.setState({ lat: position.coords.latitude }),
+                  (err) => this.setState({ errorMessage: err.message })
+            );
+      }
+
+      render() {
+            if (this.state.errorMessage && !this.state.lat) {
+                  return <div>Error: {this.state.errorMessage}</div>;
+            }
+            if (!this.state.errorMessage && this.state.lat) {
+                  return <div>Latitude: {this.state.lat}</div>;
+            }
+            return <div>Loading!</div>;
       }
 }
 
